b4adf4b459dc65b6d2059f51724c0fb123f9369c
Rhnpl <rhanielhen0@gmail.com>
2025-07-08 09:33:38 -0300

    primeira função

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, url_for
 from config import db
 from app.models.models import Usuario
-
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
 
@@ -13,6 +12,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html', titulo='Inicio')
 
 if __name__ == '__main__':
     with app.app_context():
