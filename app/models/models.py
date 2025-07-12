e70c0767bae47d9f49f03d78c21d49ab6a29cc4e
Rhnpl <rhanielhen0@gmail.com>
2025-07-11 21:22:26 -0300

    Adicionando Cursos ao Banco de Dados

--- app/models/models.py
+++ app/models/models.py
@@ -16,3 +16,19 @@ class Usuario(db.Model):
         self.senha = senha
         self.funcao = funcao
         self.ativo = True
+
+class Curso(db.Model):
+    __tablename__ = 'cursos'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(100), nullable=False)
+    carga_horaria = db.Column(db.Integer, nullable=False)
+    turno = db.Column(db.String, nullable=False)
+    ativo = db.Column(db.Boolean, nullable=False, default=True)
+
+    def __init__(self, nome, carga_horaria, turno, ativo):
+        
+        self.nome = nome
+        self.carga_horaria = carga_horaria
+        self.turno = turno
+        self.ativo = ativo
