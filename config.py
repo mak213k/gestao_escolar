4202e0b52d5356ca1f52beb94a41056e086ed119
Rhnpl <rhanielhen0@gmail.com>
2025-07-11 21:09:03 -0300

    Login e autenticação

--- config.py
+++ config.py
@@ -1,2 +1,10 @@
+import string
+import random
 from flask_sqlalchemy import SQLAlchemy
+
+tamanho = 10
+caracteres = string.ascii_letters + string.digits
+
+senha = ''.join(random.choices(caracteres, k=tamanho))
+
 db = SQLAlchemy()
--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 import os
-from flask import Flask, render_template, url_for
-from config import db
+from flask import Flask, render_template, url_for, redirect, flash, session, request
+from config import db, senha
 from app.models.models import Usuario
 
 app = Flask(__name__, template_folder='app/templates', static_folder='app/static')
+app.secret_key = senha
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 db_path = os.path.join(basedir, 'app', 'dados.db')
@@ -14,6 +15,26 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
 @app.route('/')
+def index():
+    return render_template('index.html', titulo='Inicio')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', titulo='Login')
+
+@app.route('/autenticar', methods=['POST'])
+def autenticar():
+    usuario = request.form['usuario']
+    senha = request.form['senha']
+    usuario = Usuario.query.filter((Usuario.nome == usuario)).first()
+
+    if usuario and usuario.senha == senha:
+        return redirect(url_for('index'))
+    else:
+        flash('Não foi possivel realizar o login!')
+        return redirect(url_for('login'))
+
+
 def home():
     return render_template('index.html', titulo='Inicio')
 
